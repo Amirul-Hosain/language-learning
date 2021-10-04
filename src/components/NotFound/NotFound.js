5a5563773621f9aa5e2b75842f978d8ba25911ae
Amirul-Hosain <amirulhosain77@gmail.com>
2021-10-04 22:11:34 +0600

    added redirects

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,7 +3,10 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h2>this is 404.</h2>
+            <h2 style={{
+                fontSize: '50px', textAlign: 'center',
+                margin: '10% 30%'
+            }}>Page not found 404.</h2>
         </div>
     );
 };
