d32fb8ed9b64a7b4997ce42064a318ada926f650
Amirul-Hosain <amirulhosain77@gmail.com>
2021-11-25 21:52:30 +0600

    added not found page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -3,10 +3,10 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-            <h2 style={{
+            <h1 style={{
                 fontSize: '50px', textAlign: 'center',
                 margin: '10% 30%'
-            }}>Page not found 404.</h2>
+            }}>Page not found 404.</h1>
         </div>
     );
 };
